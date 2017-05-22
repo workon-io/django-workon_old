4d14fa19c738a0af6d8ec4b30623d8586aae5d5b
Dalou <autrusseau.damien@gmail.com>
2017-05-22 16:07:37 +0200

    fix

--- workon/forms/tree.py
+++ workon/forms/tree.py
@@ -21,82 +21,84 @@ class TreeModelChoiceIterator(forms.models.ModelChoiceIterator):
 
 class TreeSelect(forms.SelectMultiple):
 
-    def render_options(self, selected_choices):
-        # Normalize to strings.
-        selected_choices = set(force_text(v) for v in selected_choices)
-        output = []
-        for choice in self.choices:
+    template_name = "workon/forms/widgets/tree.html"
 
-            if len(choice) < 3:
-                choice = choice + (None, 0, )
-            option_value, option_label, parent_id, level = choice
+    # def render_options(self, selected_choices):
+    #     # Normalize to strings.
+    #     selected_choices = set(force_text(v) for v in selected_choices)
+    #     output = []
+    #     for choice in self.choices:
 
-            if isinstance(option_label, (list, tuple)):
-                output.append(format_html('<optgroup label="{}">', force_text(option_value)))
-                for option in option_label:
-                    output.append(self.render_option(selected_choices, *option))
-                output.append('</optgroup>')
-            else:
-                output.append(self.render_option(selected_choices, option_value, option_label))
-        return '\n'.join(output)
+    #         if len(choice) < 3:
+    #             choice = choice + (None, 0, )
+    #         option_value, option_label, parent_id, level = choice
 
-    def render(self, name, value, attrs=None, choices=()):
+    #         if isinstance(option_label, (list, tuple)):
+    #             output.append(format_html('<optgroup label="{}">', force_text(option_value)))
+    #             for option in option_label:
+    #                 output.append(self.render_option(selected_choices))
+    #             output.append('</optgroup>')
+    #         else:
+    #             output.append(self.render_option(selected_choices))
+    #     return '\n'.join(output)
 
-        if value is None:
-            value = []
+    # def render(self, name, value, attrs=None, choices=()):
 
-        tree = []
-        for choice in chain(self.choices, choices):
+    #     if value is None:
+    #         value = []
 
-            if len(choice) < 3:
-                choice = choice + (None, 0, )
-            id, label, parent_id, level = choice
-            # print(id, label, parent_id, level)
-            if id:
-                node = {
-                    'id'          : str(id), #required
-                    'parent'      : str(parent_id) if level > 1 else '#', #required
-                    'text'        : label, #node text
-                    'icon'       : None, #string for custom
-                    'state'       : {
-                        'opened'    : True, #is the node open
-                        'disabled'  : False, #is the node disabled
-                        'selected'  : id in value, #is the node selected
-                    },
-                    # 'li_attr'     : {}, #attributes for the generated LI node
-                    # 'a_attr'      : {}, #attributes for the generated A node
-                }
-                tree.append(node)
-        tree = json.dumps(tree)
+    #     tree = []
+    #     for choice in chain(self.choices, choices):
 
-        final_attrs = self.build_attrs(attrs)
-        output = [format_html('<select class="hidden" multiple="multiple"{}>', flatatt(final_attrs))]
-        options = self.render_options(value)
-        if options:
-            output.append(options)
-        output.append('</select>')
+    #         if len(choice) < 3:
+    #             choice = choice + (None, 0, )
+    #         id, label, parent_id, level = choice
+    #         # print(id, label, parent_id, level)
+    #         if id:
+    #             node = {
+    #                 'id'          : str(id), #required
+    #                 'parent'      : str(parent_id) if level > 1 else '#', #required
+    #                 'text'        : label, #node text
+    #                 'icon'       : None, #string for custom
+    #                 'state'       : {
+    #                     'opened'    : True, #is the node open
+    #                     'disabled'  : False, #is the node disabled
+    #                     'selected'  : id in value, #is the node selected
+    #                 },
+    #                 # 'li_attr'     : {}, #attributes for the generated LI node
+    #                 # 'a_attr'      : {}, #attributes for the generated A node
+    #             }
+    #             tree.append(node)
+    #     tree = json.dumps(tree)
+
+    #     final_attrs = self.build_attrs(attrs)
+    #     output = [format_html('<select class="hidden" multiple="multiple"{}>', flatatt(final_attrs))]
+    #     options = self.render_options(value)
+    #     if options:
+    #         output.append(options)
+    #     output.append('</select>')
 
 
-        output += [
-            f'''<div id="id_tree_{name}" class="contrib-tree-input" ></div>''',
-            f''' <script type="text/javascript">
-                    console.log({tree})
-                    $('#id_tree_{name}').jstree(''',
-            '''    {
-                        "plugins" : [ "wholerow", "checkbox" ],
-                        'core' : {''',
-            f'''           'data' : {tree}''',
-            '''         }
-                    }).on('changed.jstree', function (e, data) {
-                var i, j, r = [];
-                for(i = 0, j = data.selected.length; i < j; i++) {
-                  r.push(data.instance.get_node(data.selected[i]).id);
-                }''',
-            f'''    $('#id_{name}').val(r).trigger('change');''',
-             ''' });
-                </script>'''
-        ]
-        return mark_safe('\n'.join(output))
+    #     output += [
+    #         f'''<div id="id_tree_{name}" class="contrib-tree-input" ></div>''',
+    #         f''' <script type="text/javascript">
+    #                 console.log({tree})
+    #                 $('#id_tree_{name}').jstree(''',
+    #         '''    {
+    #                     "plugins" : [ "wholerow", "checkbox" ],
+    #                     'core' : {''',
+    #         f'''           'data' : {tree}''',
+    #         '''         }
+    #                 }).on('changed.jstree', function (e, data) {
+    #             var i, j, r = [];
+    #             for(i = 0, j = data.selected.length; i < j; i++) {
+    #               r.push(data.instance.get_node(data.selected[i]).id);
+    #             }''',
+    #         f'''    $('#id_{name}').val(r).trigger('change');''',
+    #          ''' });
+    #             </script>'''
+    #     ]
+    #    return mark_safe('\n'.join(output))
 
 
 class TreeModelChoiceField(forms.ModelMultipleChoiceField):
@@ -106,12 +108,12 @@ class TreeModelChoiceField(forms.ModelMultipleChoiceField):
     #     level = getattr(obj, getattr(self.queryset.model._meta, 'level_attr', 'level'), 0)
     #     return u'%s %s' % (level, smart_text(obj))
 
-    def _get_choices(self):
-        if hasattr(self, '_choices'):
-            return self._choices
-        return TreeModelChoiceIterator(self)
+    # def _get_choices(self):
+    #     if hasattr(self, '_choices'):
+    #         return self._choices
+    #     return TreeModelChoiceIterator(self)
 
-    choices = property(_get_choices, forms.ChoiceField._set_choices)
+    # choices = property(_get_choices, forms.ChoiceField._set_choices)
 
 
     # def clean(self, value):
