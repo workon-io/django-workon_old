e83faad7538b58546a176bbb9da38fe0fe110a5c
Dalou <autrusseau.damien@gmail.com>
2017-07-13 18:35:29 +0200

    fix

--- workon/forms/embed.py
+++ workon/forms/embed.py
@@ -45,82 +45,16 @@ EMBED_TYPES = {
 }
 
 
-class EmbedInput(forms.widgets.ClearableFileInput):
-    template_name = 'contrib/forms/_embed_input.html'
-    change_message = "Changer"
-    empty_message = "Changer"
-    class Media:
-        css = {
-            'all': (
-                'contrib/forms/media.css',
-            )
-        }
-        js = (
-            'contrib/packages/embed.js',
-            'contrib/forms/embed.js',
-        )
-
+class EmbedInput(forms.widgets.Textarea):
 
     def __init__(self, *args, **kwargs):
         self.authorized_types = []
         if kwargs.get('authorized_types'):
             self.authorized_types = kwargs.pop('authorized_types')
-        super(EmbedInput, self).__init__(*args, **kwargs)
-
-    def render(self, name, value, attrs=None):
-        # print "EmbedInput.render : ", name, type(value), value, self.is_initial(value)
-        if value is None:
-            value = ''
-
-        field = super(EmbedInput, self).render(name, value, attrs=attrs)
-
-        in_memory = False
+        super().__init__(*args, **kwargs)
 
 
-        if isinstance(value, InMemoryUploadedFile):
-            in_memory = True
-
-        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-        checkbox_name = self.clear_checkbox_name(name)
-        checkbox_id = self.clear_checkbox_id(checkbox_name)
-        context = {
-            'only_file': len(self.authorized_types) == 1 and 'image' in self.authorized_types,
-            'required': self.is_required,
-            'field': field,
-            'name': name,
-            'value': value,
-            'id': final_attrs['id'],
-            'final_attrs': flatatt(final_attrs),
-            'change_message': self.change_message,
-            'empty_message': self.empty_message,
-            'embed_types': json.dumps(EMBED_TYPES)
-        }
-        if not self.is_required:
-            context['clear'] = CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
-        return render_to_string(self.template_name, Context(context))
-
-
-    # def decompress(self, value):
-    #     if value:
-    #         return value.split(' ')
-    #     return [None, None]
-
-    def value_from_datadict(self, data, files, name):
-        # print 'EmbedInput.value_from_datadict : ', name, (files.get(name, None), data.get(name, None))
-        if not self.is_required and CheckboxInput().value_from_datadict(
-                data, files, self.clear_checkbox_name(name)):
-            return False
-
-
-        if files:
-            return files.get(name, None)
-        else:
-            return data.get(name, None)
-
-        # return files.get(name, None), data.get(name, None)
-
-
-class EmbedField(forms.FileField):
+class EmbedField(forms.CharField):
     # widget = EmbedInput
     default_error_messages = {
         'invalid_image': "Upload a valid image. The file you uploaded was either not an image or a corrupted image.",
@@ -128,40 +62,4 @@ class EmbedField(forms.FileField):
 
     def __init__(self, *args, **kwargs):
 
-        # required = kwargs.get('required', False)
-        #widget = kwargs.pop('widget', None)
-        # widget = EmbedInput( attrs={
-        #     'class': 'contrib-media_field',
-        #     # 'data-contrib-media_field': self.image_field,
-        #     # 'style': 'display:none;',
-        # })
-        # kwargs['widget'] = widget
-        # kwargs['label'] = ""
-
-        super(EmbedField, self).__init__(*args, **kwargs)
-
-
-    def to_python(self, data):
-        # print 'MediaFormField.to_python : ', data, type(data)
-        return data
-
-    def has_changed(self, initial, data):
-        # print 'MediaFormField.has_changed:', data, type(data)
-        if data is None:
-            return super(EmbedField, self).has_changed(initial, data)
-        return True
-
-    def bound_data(self, data, initial):
-        # print 'MediaFormField.bound_data:', data, type(data), initial, type(initial)
-        if data in [False, None] and initial:
-            return initial
-        return data
-
-    def clean(self, data, initial=None):
-        if data is False:
-            if not self.required:
-                return False
-            data = None
-        if not data and initial:
-            return initial
-        return super(EmbedField, self).clean(data)
+        super().__init__(*args, **kwargs)
