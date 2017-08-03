ccfa7b0e54eaaeb2562ba2b482ff33bdbb166a4b
Dalou <autrusseau.damien@gmail.com>
2017-08-03 11:02:53 +0200

    fix

--- workon/utils/hashtag.py
+++ workon/utils/hashtag.py
@@ -1,9 +1,15 @@
 import re
 from django.utils.safestring import mark_safe
 
-__all__ = ['HASTAG_RE', 'replace_hashtags_with_hrefs', 'iter_hashtags']
+__all__ = [
+    'HASTAG_RE', 
+    'replace_hashtags_with_hrefs', 
+    'iter_hashtags',
+    'iter_hashtags_at'
+]
 
 HASTAG_RE = re.compile(r'(\#([\w\-\_\d]+))')
+HASTAG_AT_RE = re.compile(r'(\@([\w\-\_\d]+))')
 
 def replace_hashtags_with_hrefs(text, href='#{hastag}', label='#{hastag}'):
     href = href.format(hastag='\\2')
@@ -17,6 +23,10 @@ def iter_hashtags(text):
         for full_hashtag, hashtag in HASTAG_RE.findall(text):
             yield hashtag
 
+def iter_hashtags_at(text):
+    if text:
+        for full_hashtag, hashtag in HASTAG_AT_RE.findall(text):
+            yield hashtag
 
 def lazy_register(register):
 
